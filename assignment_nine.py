5b01eb4b0c329057c99574e3ca22e64c57e38aad
CJ <cj@iMac7.local>
2018-11-29 10:14:57 -0500

    Added all five cards and started to compare

--- assignment_nine.py
+++
@@ -1,9 +0,0 @@
-import deck
-
-
-def cards():
-    player_1_cards = []
-    player_2_cards = []
-
-def deal_cards():
-    
---
+++ compare.py
@@ -0,0 +1,29 @@
+import deck
+
+
+def make_a_deck():
+    deck_of_cards = deck.Deck()
+    deck_of_cards.shuffle()
+    return deck_of_cards
+
+
+def draw_a_card(deck_of_cards):
+    player_cards = []
+    for x in range(5):
+        player_cards.append(deck_of_cards.draw_a_card())
+    return player_cards
+
+
+def compare(card1, card2):
+    return card1.compared_to(card2)
+
+
+def main():
+    deck_of_cards = make_a_deck()
+    player1 = draw_a_card(deck_of_cards)
+    player2 = draw_a_card(deck_of_cards)
+    for card in player1:
+        print(card)
+
+
+main()
