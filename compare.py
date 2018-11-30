c4bc3d5552e058f2f2b80c155c80f92c779b99de
CJ <cj@iMac7.local>
2018-11-30 11:18:34 -0500

    Cpmpleted compare game deciding who wins

--- compare.py
+++ compare.py
@@ -19,11 +19,19 @@ def compare(card1, card2):
 
 
 def main():
-    deck_of_cards = make_a_deck()
-    player1 = draw_a_card(deck_of_cards)
-    player2 = draw_a_card(deck_of_cards)
-    for card in player1:
-        print(card)
+        deck_of_cards = make_a_deck()
+        player1 = draw_a_card(deck_of_cards)
+        player2 = draw_a_card(deck_of_cards)
+        for x in range(5):
+            print(player1[x])
+            print(player2[x])
+            winning_card = compare(player1[x], player2[x])
+            print("The winning card is:", winning_card)
+            print()
+        if winning_card == player1[x]:
+            print("Player 1 Wins")
+        else:
+            print("Player 2 Wins")
 
 
 main()
